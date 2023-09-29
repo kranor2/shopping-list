912c96bdf19745faae408428d054a0c6222ccbd4
Rhett North <retsrawk@gmail.com>
2023-09-29 13:59:15 -0600

    JS fixes - second attempt
    This commit includes my second attempt on manipulating the DOM to ensure
    a functional (if simple) shopping list application. Simplifying the JS
    and ensuring the scope of certain functions was appropriate made a big
    difference.

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-const listItems = document.querySelectorAll("ul");
-const textbox = document.querySelectorAll("input[type=text]");
+const list = document.querySelectorAll("ul");
+const textbox = document.querySelectorAll("input");
 const button = document.querySelectorAll("button");
 
-button.addEventListener("click", (event) => {
-    let input = document.getElementById("item");
-    input.setAttribute("value", " ");
-})
+button.addEventListener("click", () => {
+    const newItem = textbox.value;
+    textbox.value = "";
 
-const newItem = document.createElement('div');
-newItem.classList.add("newItem");
-newItem.innerHTML(<li></li>);
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const deleter = document.createElement('button');
 
-const spick = document.createElement('div');
-spick.classList.add("spick");
-spick.innerHTML(<span></span>);
-spick.appendChild(newItem);
+    listItem.appendChild(listText);
+    listText.textContent = newItem;
+    listItem.appendChild(deleter);
+    deleter.textContent = "Delete";
+    list.appendChild(listItem);
 
-const deleter = document.createElement('div');
-deleter.classList.add("deleter");
-deleter.innerHTML(<button>Delete</button>);
-
-newItem.appendChild(spick);
-newItem.appendChild(deleter);
-listItems.appendChild(newItem);
-
-deleter.addEventListener("click", (event) => {
-    remove(newItem);
-})
+    deleter.addEventListener("click", () => {
+        list.removeChild(listItem);
+    });
+    textbox.focus();
+});
